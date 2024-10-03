1fdf558944887ab32f602690298c8c1d79fd065b
nibarajith <161417555+nibarajith@users.noreply.github.com>
2024-10-03 19:11:55 +0530

    Update AdminLoginServlet.java

--- Bank/src/main/java/com/Bank/servlet/AdminLoginServlet.java
+++ Bank/src/main/java/com/Bank/servlet/AdminLoginServlet.java
@@ -17,7 +17,6 @@ import com.Bank.dao.DatabaseConnection;
 
 @WebServlet("/adminLogin")
 public class AdminLoginServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
