de29bdf91dc9a5d5ea95ca6bc89b74bc9ce7b3c1
nibarajith <161417555+nibarajith@users.noreply.github.com>
2024-10-03 19:14:39 +0530

    Update ViewBalanceServlet.java

--- Bank/src/main/java/com/Bank/servlet/ViewBalanceServlet.java
+++ Bank/src/main/java/com/Bank/servlet/ViewBalanceServlet.java
@@ -17,7 +17,6 @@ import com.Bank.dao.DatabaseConnection;
 
 @WebServlet("/ViewBalanceServlet")
 public class ViewBalanceServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
     
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
