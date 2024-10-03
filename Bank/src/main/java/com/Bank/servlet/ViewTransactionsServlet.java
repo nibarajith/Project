cebe5d509baa43370fc38630e2d1023ebde6385e
nibarajith <161417555+nibarajith@users.noreply.github.com>
2024-10-03 19:14:55 +0530

    Update ViewTransactionsServlet.java

--- Bank/src/main/java/com/Bank/servlet/ViewTransactionsServlet.java
+++ Bank/src/main/java/com/Bank/servlet/ViewTransactionsServlet.java
@@ -20,7 +20,6 @@ import com.Bank.models.Transaction;
 
 @WebServlet("/ViewTransactionsServlet")
 public class ViewTransactionsServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
