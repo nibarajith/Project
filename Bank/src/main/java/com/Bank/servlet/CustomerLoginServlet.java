60893893293bf1ebb67911ff1a1e0729b60d0811
nibarajith <161417555+nibarajith@users.noreply.github.com>
2024-10-03 19:13:08 +0530

    Update CustomerLoginServlet.java

--- Bank/src/main/java/com/Bank/servlet/CustomerLoginServlet.java
+++ Bank/src/main/java/com/Bank/servlet/CustomerLoginServlet.java
@@ -17,7 +17,6 @@ import com.Bank.dao.DatabaseConnection;
 
 @WebServlet("/customerLogin")
 public class CustomerLoginServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
