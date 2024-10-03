d1a24d5ea32c851ee5b26089053aa56b53e20445
nibarajith <161417555+nibarajith@users.noreply.github.com>
2024-10-03 19:14:24 +0530

    Update ModifyCustomerServlet.java

--- Bank/src/main/java/com/Bank/servlet/ModifyCustomerServlet.java
+++ Bank/src/main/java/com/Bank/servlet/ModifyCustomerServlet.java
@@ -13,7 +13,6 @@ import com.Bank.dao.DatabaseConnection;
 
 @WebServlet("/modifyCustomer")
 public class ModifyCustomerServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String accountNo = request.getParameter("accountNo");
